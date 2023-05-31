bdf7da5f26f140ab22d99add6662ac88c629369a
Ihor Matiev <ihor.matiev@gmail.com>
2023-05-31 03:33:10 +0300

    feat: Added validation behavior without implementation

---
+++ src/NShop.Common/NShop.Common.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,11 @@
+﻿using Mediator;
+
+namespace NShop.Common.Application.Behaviors;
+
+public class ValidationBehavior<TMessage, TResponse> : IPipelineBehavior<TMessage, TResponse> where TMessage : IMessage
+{
+    public ValueTask<TResponse> Handle(TMessage message, CancellationToken cancellationToken, MessageHandlerDelegate<TMessage, TResponse> next)
+    {
+        throw new NotImplementedException();
+    }
+}
